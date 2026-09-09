59f85b6891d309e845bff068829cb4ba1348279c
Shrijita Ghosh <shrisusghosh@gmail.com>
2026-09-09 21:34:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (83.31%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        int n=s.length();
+       
+        for(int i=0;i<n;i++)
+        {
+            
+            if(s[i]>=65&&s[i]<=90)
+            {
+                s[i]+=32;
+            }
+        }
+        return s;
+}
+};
